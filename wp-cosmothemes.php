059cd14b225f7aeecd2f8cf177a2d44e2ac13831
Jordy Meow <inscr@meow.fr>
2015-08-07 17:51:12 +0900

    Enhanced to use new features.

--- wp-cosmothemes.php
+++ wp-cosmothemes.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 Plugin Name: Cosmo Themes for Lightroom
 Description: Cosmo Themes Extension for Lightroom through the WP/LR Sync plugin.
@@ -20,15 +19,16 @@ class WPLR_Extension_CosmoThemes {
     add_action( 'wplr_update_folder', array( $this, 'update_folder' ), 10, 2 );
     add_action( 'wplr_create_collection', array( $this, 'create_collection' ), 10, 3 );
     add_action( 'wplr_update_collection', array( $this, 'update_collection' ), 10, 2 );
-
-    // Move
-    add_action( "wplr_move_folder", array( $this, 'move_collection' ), 10, 3 );
+    add_action( "wplr_move_folder", array( $this, 'move_folder' ), 10, 3 );
     add_action( "wplr_move_collection", array( $this, 'move_collection' ), 10, 3 );
+
+    // Delete
+    add_action( "wplr_remove_collection", array( $this, 'remove_collection' ), 10, 1 );
+    add_action( "wplr_remove_folder", array( $this, 'remove_folder' ), 10, 1 );
 
     // Media
     add_action( "wplr_add_media_to_collection", array( $this, 'add_media_to_collection' ), 10, 2 );
     add_action( "wplr_remove_media_from_collection", array( $this, 'remove_media_from_collection' ), 10, 2 );
-    add_action( "wplr_remove_collection", array( $this, 'remove_collection' ), 10, 1 );
 
     // Extra
     //add_action( 'wplr_reset', array( $this, 'reset' ), 10, 0 );
@@ -41,24 +41,18 @@ class WPLR_Extension_CosmoThemes {
     return $extensions;
   }
 
-  // Get the Post ID for the LR Collection $lrid (from meta)
-  function get_id_for_collection( $lrid ) {
-    global $wpdb;
-    $id = $wpdb->get_var( $wpdb->prepare( "SELECT p.post_id FROM $wpdb->postmeta p WHERE p.meta_key = %s AND p.meta_value = %d", 'wplr_to_cosmo', $lrid ) );
-    return $id;
-  }
-
   function create_collection( $collectionId, $inFolderId, $collection, $isFolder = false ) {
+    global $wplr;
 
     // If exists already, avoid re-creating
-    $hasMeta = $this->get_id_for_collection( $collectionId );
+    $hasMeta = $wplr->get_meta( "cosmothemes_gallery_id", $collectionId );
     if ( !empty( $hasMeta ) )
       return;
 
     // Get the ID of the parent collection (if any) - check the end of this function for more explanation.
     $post_parent = null;
     if ( !empty( $inFolderId ) )
-      $post_parent = $this->get_id_for_collection( $inFolderId );
+      $post_parent = $wplr->get_meta( "cosmothemes_term_id", $inFolderId );
 
     // Create the collection.
     $post = array(
@@ -68,20 +62,36 @@ class WPLR_Extension_CosmoThemes {
       'post_parent'   => $post_parent
     );
     $id = wp_insert_post( $post );
-    update_post_meta( $id, 'wplr_to_cosmo', $collectionId, true );
-    update_post_meta( $id, '_post_image_gallery', '' );
+
+    // Add a meta to retrieve easily the LR ID for that collection from a WP Post ID
+    $wplr->set_meta( 'cosmothemes_gallery_id', $collectionId, $id );
+
+    // Associate this portfolio to a category
+    $parentTermId = $wplr->get_meta( "cosmothemes_term_id", $inFolderId );
+    if ( $parentTermId ) {
+      $term = get_term_by( 'term_id', $parentTermId, 'gallery-category' );
+      if ( !empty( $term ) )
+        wp_set_post_terms( $id, $term->term_id, 'gallery-category' );
+    }
   }
 
   function create_folder( $folderId, $inFolderId, $folder ) {
-    // Well, we can say that a folder is a collection (we could have use a taxonomy for that too)
-    // Let's keep it simple and re-use the create_collection with an additional parameter to avoid having content.
-    //$this->create_collection( $folderId, $inFolderId, $folder, true );
+    global $wplr;
+    $parentTermId = $wplr->get_meta( "cosmothemes_term_id", $inFolderId );
+    $result = wp_insert_term( $folder['name'], 'gallery-category', array( 'parent' => $parentTermId ) );
+    if ( is_wp_error( $result ) ) {
+      error_log( "Issue while creating the folder " . $folder['name'] . "." );
+      error_log( $result->get_error_message() );
+      return;
+    }
+    $wplr->set_meta( 'cosmothemes_term_id', $folderId, $result['term_id'] );
   }
 
   // Updated the collection with new information.
   // Currently, that would be only its name.
   function update_collection( $collectionId, $collection ) {
-    $id = $this->get_id_for_collection( $collectionId );
+    global $wplr;
+    $id = $wplr->get_meta( "cosmothemes_gallery_id", $collectionId );
     $post = array( 'ID' => $id, 'post_title' => wp_strip_all_tags( $collection['name'] ) );
     wp_update_post( $post );
   }
@@ -89,24 +99,29 @@ class WPLR_Extension_CosmoThemes {
   // Updated the folder with new information.
   // Currently, that would be only its name.
   function update_folder( $folderId, $folder ) {
-    $this->update_collection( $folderId, $folder );
+    global $wplr;
+    $termId = $wplr->get_meta( "cosmothemes_term_id", $folderId );
+    wp_update_term( $termId, 'gallery-category', array( 'name' => $folder['name'] ) );
   }
 
   // Moved the collection under another folder.
   // If the folder is empty, then it is the root.
   function move_collection( $collectionId, $folderId, $previousFolderId ) {
-    $post_parent = null;
-    if ( !empty( $folderId ) )
-      $post_parent = $this->get_id_for_collection( $folderId );
-    $id = $this->get_id_for_collection( $collectionId );
-    $post = array( 'ID' => $id, 'post_parent' => $post_parent );
-    wp_update_post( $post );
+    global $wplr;
+    $galleryId = $wplr->get_meta( "cosmothemes_gallery_id", $collectionId );
+    $parentTermId = empty( $folderId ) ? null : $wplr->get_meta( "cosmothemes_term_id", $folderId );
+    $previousTermId = empty( $previousFolderId ) ? null : $wplr->get_meta( "cosmothemes_term_id", $previousFolderId );
+
+    // Remove the previous term (category) and add the new one
+    wp_remove_object_terms( $galleryId, $previousTermId, 'gallery-category' );
+    wp_set_post_terms( $galleryId, $parentTermId, 'gallery-category' );
   }
 
   // Added meta to a collection.
   // The $mediaId is actually the WordPress Post/Attachment ID.
   function add_media_to_collection( $mediaId, $collectionId, $isRemove = false ) {
-    $id = $this->get_id_for_collection( $collectionId );
+    global $wplr;
+    $id = $wplr->get_meta( "cosmothemes_gallery_id", $collectionId );
     $str = get_post_meta( $id, '_post_image_gallery', TRUE );
     $ids = !empty( $str ) ? explode( ',', $str ) : array();
     $index = array_search( $mediaId, $ids, false );
@@ -129,14 +144,30 @@ class WPLR_Extension_CosmoThemes {
 
   // Remove media from the collection.
   function remove_media_from_collection( $mediaId, $collectionId ) {
+    global $wplr;
     $this->add_media_to_collection( $mediaId, $collectionId, true );
+
+    // Need to delete the featured image if it was this media
+    $postId = $wplr->get_meta( "cosmothemes_gallery_id", $collectionId );
+    $thumbnailId = get_post_meta( $postId, '_thumbnail_id', -1 );
+    if ( $thumbnailId == $mediaId )
+      delete_post_meta( $postId, '_thumbnail_id' );
   }
 
   // The collection was deleted.
   function remove_collection( $collectionId ) {
-    $id = $this->get_id_for_collection( $collectionId );
+    global $wplr;
+    $id = $wplr->get_meta( "cosmothemes_gallery_id", $collectionId );
     wp_delete_post( $id, true );
-    delete_post_meta( $id, 'wplr_to_cosmo' );
+    $wplr->delete_meta( 'cosmothemes_gallery_id', $collectionId );
+  }
+
+  // Delete the folder.
+  function remove_folder( $folderId ) {
+    global $wplr;
+    $id = $wplr->get_meta( "cosmothemes_term_id", $folderId );
+    wp_delete_term( $id, 'gallery-category' );
+    $wplr->delete_meta( 'cosmothemes_term_id', $folderId );
   }
 }
 
